f726318a4fbecc6a3bc43c4acda4a7d7775baf58
roikatz16 <kroi@post.bgu.ac.il>
2020-06-03 11:29:29 +0300

    omg

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,24 @@ var express = require("express");
 var router = express.Router();
 
 //Authentivation all incoming request
-router.use((req, res, next)=> {
-    if(req.session && req.session.id){
+router.use((req, res, next) => {
+    if (req.session && req.session.id) {
         const id = req.session.id;
-        
-    }
-}
+        const user = checkIdOnDb(id);
 
-)
+        if (user){
+            req.user = user;
+            next();
+        }
+    }
+    req.sendStatus(401);
+});
+
+router.get("/user/recipeInfo/{ids}", (req, res) => {
+    const ids = req.params.ids;
+    const user_name = req.user;
+    getUserInfoOnRecipes(user_name, ids); 
+});
+
+modules.exports = router;
+
---
+++ routes/utils/search_recipes.js
@@ -0,0 +1,15 @@
+
+
+function extrasSearchResultIds(search_response){
+    let recipes = search_respose.data.results;
+    recipes_id_list = [];
+    recipes.map((recipe) => {
+        console.log(recipe.title);
+        recipes_id_list.push(recipe.id);
+    });
+
+    return recipes_id_list;
+}
+
+// exports.searchForRecipes = searchForRecipes;
+exports.extrasSearchResultIds = extrasSearchResultIds;

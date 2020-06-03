99144aa52f606be39c2ee65e65ca818d63fcb2f9
Michal Talmor <talmormi@post.bgu.ac.il>
2020-06-03 11:40:44 +0300

    update recipes

---
+++ routes/recipes.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+
+const search_util = require("./utils/search_recipes");
+
+router.use((req, res, next) => {
+    console.log("Recipes route");
+    next();
+});
+
+//routes ------------------- PROBLEMS!!!!!!!!!!!!!
+router.get("/search/query/:searchQuery/amount/:num", (req, res) => {
+    const { searchQuery, num } = req.params;
+    search_params = {};
+    search_params.query = searchQuery;
+    search_params.number
+})
+
+//check if queries params exists
+console.log(req.query);
+search_util.extractQueriesParams(req.query, num, search_params);
+
+search_util
+    .seachForRecipes(searchQuery, num, search_params)
+    .then(info_array) => res.send(info_array))
+    .catch (error) => {
+        console.log(error);
+        res.sendStatus(500);
+    });
+});

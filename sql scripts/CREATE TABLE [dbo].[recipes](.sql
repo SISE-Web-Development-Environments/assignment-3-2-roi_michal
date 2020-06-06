f16c5b05af20e3227ff6e409bdd2485bde7061be
roikatz16 <kroi@post.bgu.ac.il>
2020-06-07 00:40:10 +0300

    sql recipes

--- sql scripts/CREATE TABLE [dbo].[recipes](.sql
+++ sql scripts/CREATE TABLE [dbo].[recipes](.sql
@@ -1,12 +1,11 @@
 CREATE TABLE [dbo].[recipes](
 	[recipe_id] [UNIQUEIDENTIFIER] PRIMARY KEY NOT NULL default NEWID(),
-	[author] [varchar] NOT NULL,
-	[recipe_name] [varchar](300) NOT NULL,
-	[image_url] [varchar](500) NOT NULL,
+	[author] [varchar] (30) NOT NULL,
+	[recipe_name] [varchar] (300) NOT NULL,
+	[image_url] [varchar] (500) NOT NULL,
 	[prep_time] [int] NOT NULL,
 	[vegan] [int] NOT NULL,
 	[vegeterian] [int] NOT NULL,
 	[gluten_free] [int] NOT NULL,
 	[servings] [int] NOT NULL
-
 )
